b8b0ec461c9bec5af5e7338d201a1270631cdf53
caslu <lucas1.martin@epitech.eu>
2024-05-30 20:00:16 +0400

    inital commit : setup app and add a title

---
+++ my_blind_test/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>My blind test</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 50,
+    color: 'lightgreen',
+    fontWeight: 'bold',
+  }
+});
